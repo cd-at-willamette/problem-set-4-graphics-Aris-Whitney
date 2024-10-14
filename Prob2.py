ec78d6c03692eb23416b2f64b5a432372ef444bd
Aris-Whitney <Acwhitney@willamette.edu>
2024-10-13 17:40:51 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,25 +1,48 @@
 ########################################
-# Name:
-# Collaborators:
-# Estimated time spent (hrs):
+# Name:Aris Whitney
+# Collaborators:N/A
+# Estimated time spent (hrs):1
 ########################################
 
 from pgl import GWindow, GRect
 
-WIDTH = 800
-HEIGHT = 600
-BRICK_WIDTH = 40
-BRICK_HEIGHT = 20
-BRICKS_IN_BASE = 15
 
+WIDTH = 800         
+HEIGHT = 600         
+BRICK_WIDTH = 40     
+BRICK_HEIGHT = 20    
+BRICKS_IN_BASE = 15 
 def draw_pyramid():
-    """ 
-    Draws a pyramid of bricks centered on the screen with a height of BRICKS_IN_BASE. 
-    """
-
+    # Create the graphics window
     gw = GWindow(WIDTH, HEIGHT)
 
-    # You got it from here
+    # Calculate the total height of the pyramid
+    total_height = BRICK_HEIGHT * BRICKS_IN_BASE
+
+    # starting y position 
+    start_y = (HEIGHT - total_height) // 2
+
+    # Draw the pyramid
+    for row in range(BRICKS_IN_BASE):
+        # Calculates the number of bricks in this row 
+        num_bricks = row + 1  # Increases the number of bricks as we go down
+
+        # starting x position
+        start_x = (WIDTH - (BRICKS_IN_BASE * BRICK_WIDTH)) // 2 + (BRICKS_IN_BASE - num_bricks) * (BRICK_WIDTH // 2)
+        for brick in range(num_bricks):
+            x = start_x + brick * BRICK_WIDTH
+            y = start_y + (BRICKS_IN_BASE - 1 - row) * BRICK_HEIGHT  # Position rows from bottom to top
+            brick_rect = GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT)
+            brick_rect.set_filled(True)
+            brick_rect.set_fill_color("brown")
+            gw.add(brick_rect)
+
+
+
+
+
+if __name__ == '__main__':
+    draw_pyramid()
 
 
 
